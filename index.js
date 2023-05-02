410c287c93bb3d27452d15a91b4ac5a5641f3309
kamelmahmud0408 <mahmudulhasankamel1994@gmail.com>
2023-05-01 22:21:36 -0700

    added chefdata

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
-const app = express()
+const app = express();
+// const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+const chefdata = require('./data/chefdata.json');
+
+// app.use(cors());
+
 app.get('/', (req, res) => {
-  res.send('Hello World! hi i am kamel')
+  res.send('chef Data is coming')
+})
+
+app.get('/chefdata', (req, res) => {
+    res.send(chefdata);
 })
 
 app.listen(port, () => {
